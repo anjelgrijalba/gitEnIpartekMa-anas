94e5300d750397a6acb6b221a4ccdac30fd8e439
anjel <anggri@gmail.com>
2018-02-09 12:56:05 +0100

    añadida funcion suma en biblioteca de pruebas

---
+++ biblioteca/pruebas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace biblioteca
+{
+    public class pruebas
+    {
+        public static int sumar (int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
